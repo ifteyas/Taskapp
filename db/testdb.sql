294a2c7213d36ee04bfead811f3253ff5c70fab5
ifteyas <145397850+ifteyas@users.noreply.github.com>
2023-12-14 08:45:49 +0530

    final submission

---
+++ db/testdb.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2023 at 04:01 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'test'),
+(2, 'yoyo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `title`, `description`, `category_id`) VALUES
+(1, 'hi', 'hi', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Task Management Web Application
+
+This is a simple Task Management web application implemented in PHP with a MySQL database. It allows you to manage tasks and categories.
+
+## Features
+
+- View existing tasks and categories.
+- Add new tasks with titles, descriptions, and categories.
+- Add new categories.
+
+## Getting Started
+
+1. **Database Configuration:**
+   - Ensure you have a MySQL server set up.
+   - Update the database configuration in the PHP script (`index.php`) with your MySQL server details (`$servername`, `$username`, `$password`, `$dbname`).
+
+2. **Run the Application:**
+   - Place the PHP script (`index.php`) in your web server's root directory.
+   - Access the application through a web browser (e.g., `http://localhost/index.php`).
+
+3. **Bootstrap Integration:**
+   - The application uses Bootstrap for styling. Bootstrap CSS and JavaScript are linked in the HTML (`index.php`) from CDN.
+
+4. **Functionality:**
+   - View existing tasks and categories on the home page.
+   - Add new tasks and categories using the provided forms.
+
+## Screenshots
+
+- <img src="screenshots\app.png" width="100%" />>
+- <img src="screenshots\Database.png" width="100%" />
+
+## Dependencies
+
+- Bootstrap 4.5.2
+
+## Contributing
+
+Contributions are welcome! Feel free to fork the repository, make changes, and submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
